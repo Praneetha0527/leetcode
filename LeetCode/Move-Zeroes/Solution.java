e551b49f49df3318f56162519f050233310dd22c
PRANEETHA R S <praneethars.23csd@kongu.edu>
2026-09-24 11:24:16 +0530

    Add solution for Move Zeroes

---
+++ LeetCode/Move-Zeroes/Solution.java
@@ -0,0 +1,13 @@
+1class Solution {
+2    public void moveZeroes(int[] nums) {
+3        int j=0;
+4        for(int i=0;i<nums.length;i++){
+5            if(nums[i]!=0){
+6                int temp=nums[i];
+7                nums[i]=nums[j];
+8                nums[j]=temp;
+9                j++;
+10            }
+11        }
+12    }
+13}
